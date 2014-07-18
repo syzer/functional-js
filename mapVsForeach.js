fd878fcce1bbc92052092e8a7ef0c9195f7d8f1a
syzer <syzer3@gmail.com>
2014-07-18 15:24:40 +0200

    map vs - foreach

---
+++ mapVsForeach.js
@@ -0,0 +1,34 @@
+// 42% faster!!!
+_ = require('lodash');
+var data =_.map(_.range(10000), function(el){
+    var obj = {};
+    obj[el] = 'wat';
+    obj[111]= 'man';
+    obj[666] = '!';
+    obj.name = 'test';
+    return obj;
+});
+
+var data2 = _.cloneDeep(data);
+
+function isNumeric(num) {
+    return !_.isNaN(parseInt(num));
+}
+
+
+console.time('map');
+data2 = data2.map(function (article, i) {
+    return _.omit(article, function (value, key) {
+        return isNumeric(key);
+    });
+});
+console.timeEnd('map');
+
+console.time('forEach');
+data.forEach(function (article, i) {
+    data[i] = _.omit(article, function (value, key) {
+        return isNumeric(key);
+    });
+});
+console.timeEnd('forEach');
+console.log(data[0]);
