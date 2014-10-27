8ddb9fa4e1dfc277ea2b0856a679595451560ecb
syzer <syzer3@gmail.com>
2014-10-27 14:42:37 +0100

    example about curring, aka callback first monadsExample on express.js

--- currying.js
+++ currying.js
@@ -195,3 +195,24 @@ getIncompleteTaskSummaries3 = function (memberName) {
     });
 };
 //getIncompleteTaskSummaries3('Scott').then(print);
+
+
+//One may solve it using very reusable code: an query optimizer.. like SQL optimizes JOIN orders
+//Picture that:
+
+    var getIncompleteTaskSummaries = R.optimize(
+        then(R.get('tasks'))
+            .then(R.filter(R.propEq('username', membername)))
+            .then(R.reject(R.propEq('complete', true)))
+            .then(R.map(R.pick(['id', 'dueDate', 'title', 'priority'])))
+            .then(R.sortBy(R.get('dueDate')))
+    );
+// this runs SQL-like optimizer // and then runs full transformation/reduction
+getIncompleteTaskSummaries(fetchData());
+
+
+//That allows you to separate concerns(SOLID all the way)...
+//what needs to be done is different
+//than what would be the fastest possible execution plan giving CURRENT resources
+
+//Please, note that given different data locality(ex: some data in remote servers) the execution order may be different
---
+++ my_talk_fp_in_js/monadsExample.js
@@ -0,0 +1,27 @@
+/**
+ * Created by syzer on 10/27/2014.
+ */
+
+// monads for dynamic type safety
+exports.index = function (req, res) {
+    // here db call to get users
+    var users = [
+        {id: 1},
+        {id: 2, email: 'some@that.loggedin.com'}
+    ];
+
+    res.status(200, users);
+};
+
+exports.index = function (req, res) {
+    // here db call to get users
+    var users = [
+        {id: 1},
+        {id: 2, email: 'some@that.loggedin.com'}
+    ];
+
+    res.status(200).json(users);
+};
+
+
+
