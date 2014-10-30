56bbc027d220f36a660abef1a1059d2aea5e945c
syzer <syzer3@gmail.com>
2014-10-30 14:36:01 +0100

    preparing map reduce exercise

---
+++ my_talk_fp_in_js/mapReduceWarmup.js
@@ -0,0 +1,43 @@
+/**
+ * Created by syzer on 10/30/2014.
+ */
+var isTheArea = [
+    'Urban',
+    'Peri-urban',
+    'Rural',
+    'NS'
+];
+
+function addNameProp(el) {
+    return {name: el}
+}
+
+// add map call to get
+[
+    { name: 'Urban' },
+    { name: 'Peri-urban' },
+    { name: 'Rural' },
+    { name: 'NS' }
+]
+
+
+
+
+console.log(isTheArea);
+//.map(addNameProp);
+
+var taskConfig,todos, deferred, dispatcher={addTask:function(){}}, task, i;
+
+_.times(taskConfig.times, function(n) {
+    deferred = defer();
+    dispatcher.addTask(task, deferred);
+    todos.push(deferred.promise);
+});
+
+// vs
+
+for (i = 1; i <= taskConfig.times; i++) {
+    deferred = defer();
+    dispatcher.addTask(task, deferred);
+    todos.push(deferred.promise);
+}
