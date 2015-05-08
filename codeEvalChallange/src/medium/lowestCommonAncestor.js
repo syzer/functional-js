653dcac89a4ccf29e60f61ccff72e541fad9d6c0
syzer <syzer3@gmail.com>
2015-05-08 15:28:17 +0200

    lowest common ansestor

---
+++ codeEvalChallange/src/medium/lowestCommonAncestor.js
@@ -0,0 +1,68 @@
+"use strict";
+var _ = require('lodash');
+var lib = require('./../lib/lib')(_);
+_.mixin(lib);
+
+// parent child
+var tree = [
+    [30, 30],
+    [30, 8],
+    [30, 52],
+    [8, 3],
+    [8, 20],
+    [20, 10],
+    [20, 29]
+];
+
+function findParents(num1, walked) {
+    if (num1 === 30) {  // top magic number
+        if (_.isEmpty(walked)) {
+            return [30]
+        }
+        return walked;
+    }
+
+    var parent = tree.filter(function (el) {
+        return el[1] === num1;
+    })[0][0];
+
+    walked.push(parent);
+
+    return findParents(parent, walked);
+}
+
+function lowestCommonAncestor(arr) {
+    var parents1 = findParents(arr[0], []);
+    var parents2 = findParents(arr[1], []);
+
+    var lowestCommonAncestor = _.flowRight(_.first, _.intersection);
+
+    return lowestCommonAncestor(parents1, parents2);
+}
+
+function prepare(line) {
+    return lowestCommonAncestor(line.split(' ').map(Number));
+}
+
+function run(input) {
+    return readLines(input, prepare);
+}
+
+function runAll(input) {
+    return prepare(input);
+}
+
+function readLines(input, lineCallback) {
+    return input
+        .split('\n')
+        .map(function (line, i) {
+            if ('' === line) {
+                return;
+            }
+            return lineCallback(line, i);
+        })
+        .join('\n');
+}
+
+module.exports.run = run;
+module.exports.runAll = runAll;
---
+++ codeEvalChallange/test/current/lowestCommonAnsestorTest.js
@@ -0,0 +1,109 @@
+/**
+ * https://www.codeeval.com/open_challenges/11/
+ *
+ *     30
+        |
+      ____
+     |   |
+     8   52
+     |
+    ____
+   |   |
+   3  20
+       |
+      ____
+     |   |
+     10  29
+ *
+ */
+var expect = require('chai').expect;
+var SRC_DIR = './../../src/medium/';
+
+var lib = require(SRC_DIR + 'lowestCommonAncestor');
+var _ = require('lodash');
+
+var input = [
+    '8 52',
+    '3 29',
+    '20 29',
+    //'30 3',
+    //'52 30',
+    //'10 3',
+    //'3 10'
+].join('\n');
+
+var output = [
+    '30',
+    '8',
+    '20',
+    //'30',
+    //'30',
+    //'8',
+    //'8'
+].join('\n');
+
+var inputFull = [
+    '29 10',
+    '30 29',
+    '3 30',
+    '20 3',
+    '20 10',
+    '52 30',
+    '20 10',
+    '8 20',
+    '29 52',
+    '20 29',
+    '52 30',
+    '8 52',
+    '30 29',
+    '3 20',
+    '8 20',
+    '10 3',
+    '20 29',
+    '8 10',
+    '20 52'
+].join('\n');
+
+var outputFull = [
+    '20',
+    '30',
+    '30',
+    '8',
+    '8',
+    '30',
+    '8',
+    '30',
+    '30',
+    '8',
+    '30',
+    '30',
+    '30',
+    '8',
+    '30',
+    '8',
+    '8',
+    '30',
+    '30'
+].join('\n');
+
+describe('lowestCommonAncestor', function () {
+
+    afterEach(function (done) {
+        setTimeout(done, 60);
+    });
+
+    it('can run', function (done) {
+        var out = lib.run(input);
+        console.log('out:\n', out);
+        expect(out).eql(output);
+        done();
+    });
+
+    //it('can run full', function (done) {
+    //    var out = lib.run(inputFull);
+    //    console.log('out:\n', out);
+    //    expect(out).eql(outputFull);
+    //    done();
+    //});
+
+});
