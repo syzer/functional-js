ef738266748b226da77e46181469a4171900622a
syzer3@gmail.com <syzer3@gmail.com>
2015-11-16 10:16:09 +0100

    object observe

--- ecm6.js
+++ ecm6.js
@@ -1,13 +1,34 @@
-//TODO when new version relesed
+var Promise = require('bluebird');
+//import * as Promise from 'bluebird';
+
 
 (function loop(sum, stop) {
     if (sum < stop) {
         return Promise.delay(250).then(() => {
-                sum++;
-        console.log(sum);
-        return loop(sum, stop);
-    });
-}
+            sum++;
+            console.log(sum);
+            return loop(sum, stop);
+        });
+    }
 })(0, 10).then(() => console.log('Done'));
 
+
+// angular
+// A model can be a simple vanilla object
+var todoModel = {
+    label: 'Default',
+    completed: false
+};
+
+function observer(changes) {
+    changes.forEach((change, i) => {
+        console.log('what property changed? ' + change.name);
+        console.log('how did it change? ' + change.type);
+        console.log('whats the current value? ' + change.object[change.name]);
+        console.log(change); // all changes
+    });
+}
+
+Object.observe(todoModel, observer);
+todoModel.label = 'Buy some more milk';
 
