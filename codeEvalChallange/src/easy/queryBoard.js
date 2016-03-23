7f734e0decccbebe70c73b9879ac920fa430819d
syzer3@gmail.com <syzer3@gmail.com>
2016-03-23 16:17:22 -0400

    query board started

---
+++ codeEvalChallange/src/easy/queryBoard.js
@@ -0,0 +1,19 @@
+const run = (line) =>
+    line.split('|').map(col =>
+            col.trim().split(' ').map(el => parseInt(el))
+        )
+        .reduce((max, curr) =>
+            max.map((el, i) => max[i] > curr[i] ? max[i] : curr[i])
+        ).join(' ')
+
+const processFile = (inputFile) => {
+    const fs = require('fs'),
+        readline = require('readline'),
+        inStream = fs.createReadStream(inputFile),
+        outStream = new (require('stream'))(),
+        rl = readline.createInterface(inStream, outStream)
+
+    rl.on('line', line => console.log(line))
+}
+
+processFile('./tempFile.txt')
