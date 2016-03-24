921a094013ab30eef91e8d53171d4a05b661c385
syzer3@gmail.com <syzer3@gmail.com>
2016-03-24 10:09:02 -0400

    racing chars

--- codeEvalChallange/src/easy/racingChars.js
+++ codeEvalChallange/src/easy/racingChars.js
@@ -1,44 +1,43 @@
 'use strict';
 
-let arr = Array.from({length: 256}, () => 0)
-    .map(row =>
-        Array.from({length: 256}, () => 0)
-    )
+let pos = 9
+let went = false
 
-const add = (a, b) => a + b
+const go = (i) => {
+    if (i === pos) {
+        return '|'
+    }
+    if (pos < i) {
+        pos++
+        return "\\"
+    }
+    pos--
+    return '/'
+}
+
+
 const parseLine = (line) => {
-    const out = line.split(' ')
-    const command = out[0]
-    const x = parseInt(out[1], 10)
-    const y = parseInt(out[2], 10)
-    return {command, x, y}
-}
-
-const modifyArr = (com) => {
-    return {
-        SetCol () {
-            arr = arr.map(row => {
-                row[com.x] = com.y
-                return row
-            })
-        },
-        SetRow () {
-            arr[com.x] = Array.from({length: 256}, () => com.y)
-        },
-        QueryCol () {
-            console.log(arr.map(row => row[com.x]).reduce(add))
-        },
-        QueryRow () {
-            console.log(arr[com.x].reduce(add))
+    went = false
+    return line.split('').map((c, i) => {
+        if ('#' === c) {
+            return '#'
         }
-    }[com.command]()
+        if ('C' === c) {
+            went = true
+            return go(i)
+        }
+        if ('_' === c) {
+            if (went) {
+                return '_'
+            }
+            went = true
+            return go(i)
+        }
+    }).join('')
 }
 
-// [[0, 0, 0],
-//  [0, 1 , 1],
-//  [1, 1, 0]]
 const run = (lines) => {
-    lines.split('\n').map(parseLine).map(modifyArr)
+    return lines.split('\n').map(parseLine)
 }
 
 module.exports.run = run
