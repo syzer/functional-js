23bcada89e13c2e3c387ece86bc99619e8c96dd1
syzer3@gmail.com <syzer3@gmail.com>
2016-03-23 22:01:13 -0400

    query board passes even if test says nope

--- codeEvalChallange/src/easy/queryBoard.js
+++ codeEvalChallange/src/easy/queryBoard.js
@@ -1,19 +1,43 @@
-const run = (line) =>
-    line.split('|').map(col =>
-            col.trim().split(' ').map(el => parseInt(el))
-        )
-        .reduce((max, curr) =>
-            max.map((el, i) => max[i] > curr[i] ? max[i] : curr[i])
-        ).join(' ')
+'use strict';
 
-const processFile = (inputFile) => {
-    const fs = require('fs'),
-        readline = require('readline'),
-        inStream = fs.createReadStream(inputFile),
-        outStream = new (require('stream'))(),
-        rl = readline.createInterface(inStream, outStream)
+let arr = Array.from({length: 256}, () => 0).map(row =>
+    Array.from({length: 256}, () => 0)
+)
 
-    rl.on('line', line => console.log(line))
+const add = (a, b) => a + b
+const parseLine = (line) => {
+    const out = line.split(' ')
+    const command = out[0]
+    const x = parseInt(out[1], 10)
+    const y = parseInt(out[2], 10)
+    return {command, x, y}
 }
 
-processFile('./tempFile.txt')
+const modifyArr = (com) => {
+    return {
+        SetCol () {
+            arr = arr.map(row => {
+                row[com.x] = com.y
+                return row
+            })
+        },
+        SetRow () {
+            arr[com.x] = Array.from({length: 256}, () => com.y)
+        },
+        QueryCol () {
+            console.log(arr.map(row => row[com.x]).reduce(add))
+        },
+        QueryRow () {
+            console.log(arr[com.x].reduce(add))
+        }
+    }[com.command]()
+}
+
+// [[0, 0, 0],
+//  [0, 1 , 1],
+//  [1, 1, 0]]
+const run = (lines) => {
+    lines.split('\n').map(parseLine).map(modifyArr)
+}
+
+module.exports.run = run
--- codeEvalChallange/test/current/queryBoardTest.js
+++ codeEvalChallange/test/current/queryBoardTest.js
@@ -1,5 +1,5 @@
 /**
- * https://www.codeeval.com/open_challenges/87/ 
+ * https://www.codeeval.com/open_challenges/87/
  * Query Board
 
  Challenge Description:
@@ -38,16 +38,15 @@
  34
  1792
  3571
-
  *
  */
 const expect = require('chai').expect;
 const SRC_DIR = './../../src/easy/';
 
-const lib = require(SRC_DIR + 'panaceaHexToNum')
+const lib = require(SRC_DIR + 'queryBoard')
 const _ = require('lodash')
 
-var input = [
+const input = [
     'SetCol 32 20',
     'SetRow 15 7',
     'SetRow 16 31',
@@ -60,14 +59,167 @@ var input = [
     'QueryCol 2'
 ].join('\n')
 
-var output = [
+const output = [
     '5118',
     '34',
     '1792',
     '3571'
-].join('\n');
+].join('\n')
 
-describe('panaceaHexToNumber', () => {
+const fullInput = [
+    'SetRow 25 16',
+    'QueryCol 57',
+    'QueryRow 220',
+    'QueryRow 46',
+    'SetRow 73 12',
+    'QueryRow 178',
+    'QueryCol 7',
+    'QueryCol 146',
+    'QueryRow 230',
+    'QueryRow 10',
+    'QueryCol 254',
+    'SetCol 161 23',
+    'SetRow 40 25',
+    'QueryCol 178',
+    'SetCol 110 29',
+    'SetCol 0 6',
+    'SetRow 204 2',
+    'QueryRow 206',
+    'QueryCol 222',
+    'SetRow 242 18',
+    'QueryCol 70',
+    'QueryRow 12',
+    'SetRow 68 6',
+    'SetRow 107 13',
+    'QueryRow 81',
+    'QueryRow 66',
+    'QueryCol 243',
+    'QueryCol 252',
+    'QueryRow 234',
+    'QueryRow 176',
+    'QueryCol 240',
+    'QueryRow 190',
+    'QueryRow 104',
+    'QueryRow 195',
+    'SetRow 150 18',
+    'QueryCol 66',
+    'SetRow 42 30',
+    'QueryCol 218',
+    'QueryRow 35',
+    'SetRow 49 30',
+    'SetRow 160 6',
+    'QueryCol 64',
+    'SetCol 205 1',
+    'QueryCol 199',
+    'SetRow 126 25',
+    'SetRow 158 3',
+    'QueryCol 24',
+    'QueryRow 3',
+    'QueryRow 95',
+    'SetRow 238 5',
+    'QueryRow 217',
+    'SetRow 227 2',
+    'QueryRow 215',
+    'SetRow 229 31',
+    'SetCol 85 29',
+    'QueryCol 76',
+    'SetCol 166 0',
+    'QueryCol 210',
+    'SetRow 96 30',
+    'QueryCol 78',
+    'SetCol 123 15',
+    'SetRow 152 8',
+    'QueryRow 197',
+    'SetRow 65 1',
+    'QueryCol 172',
+    'SetCol 28 15',
+    'QueryCol 73',
+    'SetCol 155 31',
+    'SetRow 97 18',
+    'QueryRow 174',
+    'SetRow 78 21',
+    'QueryCol 71',
+    'SetRow 247 9',
+    'SetRow 240 17',
+    'QueryRow 74',
+    'QueryCol 234',
+    'QueryRow 0',
+    'SetCol 244 20',
+    'QueryCol 216',
+    'SetRow 95 15',
+    'QueryRow 248',
+    'SetRow 188 18',
+    'QueryCol 123',
+    'SetCol 130 24',
+    'QueryCol 236',
+    'SetCol 152 9',
+    'QueryRow 41',
+    'SetCol 121 31',
+    'QueryCol 90',
+    'QueryCol 155',
+    'QueryRow 170',
+    'SetCol 171 0',
+    'QueryRow 161',
+    'QueryCol 153',
+    'QueryRow 185',
+    'QueryCol 90',
+    'SetRow 219 25',
+    'SetCol 151 9',
+    'SetRow 127 10',
+    'SetCol 208 28',
+    'SetRow 175 16',
+    'QueryRow 248',
+    'SetCol 69 9',
+    'QueryRow 67',
+    'QueryRow 117',
+    'QueryCol 54',
+    'QueryRow 233',
+    'SetRow 226 21',
+    'QueryRow 85',
+    'QueryRow 135',
+    'SetCol 44 10',
+    'SetRow 248 5',
+    'QueryCol 92',
+    'QueryCol 67',
+    'QueryRow 117',
+    'QueryCol 54',
+    'SetCol 161 0',
+    'SetRow 226 9',
+    'QueryCol 85',
+    'SetCol 251 17',
+    'QueryRow 175',
+    'SetCol 152 10',
+    'SetRow 92 1',
+    'SetRow 67 5',
+    'QueryRow 117',
+    'SetRow 54 3',
+    'SetCol 161 7',
+    'QueryCol 226',
+    'QueryRow 85',
+    'QueryCol 135',
+    'SetRow 175 27',
+    'QueryCol 248',
+    'QueryCol 92',
+    'QueryRow 67',
+    'SetCol 107 9',
+    'QueryRow 54',
+    'SetRow 233 8',
+    'SetCol 100 15',
+    'QueryCol 85',
+    'SetCol 251 25',
+    'SetRow 175 21',
+    'QueryRow 226',
+    'QueryRow 85',
+    'QueryCol 135',
+    'SetCol 152 17',
+    'QueryRow 92',
+    'SetRow 67 1',
+    'QueryCol 117',
+    'QueryRow 54',
+    'SetRow 233 16'
+].join('\n')
+
+describe('queryBoard', () => {
 
     it('can run', () => {
         var out = lib.run(input)
