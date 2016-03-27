c47bac6087aaa49870091f4600ea71b6c8eeb596
syzer3@gmail.com <syzer3@gmail.com>
2016-03-27 15:22:22 -0400

    date diff solved

--- codeEvalChallange/src/easy/dateTimeDiff.js
+++ codeEvalChallange/src/easy/dateTimeDiff.js
@@ -1,44 +1,25 @@
 'use strict';
 
-var str = '14:01:57 12:47:11'
-var arr = str.split(' ').sort().map(el => el.split(':'))
+const pad = (num) => num < 10 ? `0${num}` : `${num}`
 
-var a = new Date()
-a.setHours(arr[0][0])
-a.setMinutes(arr[0][1])
-a.setSeconds(arr[0][2])
+const timeDiff = (dates) =>
+    dates.map(date => {
+        let d = new Date()
+        d.setHours(date[0])
+        d.setMinutes(date[1])
+        d.setSeconds(date[2])
+        return d
+    }).reduce((a,b) => {
+        const c = new Date(b-a)
+        return [c.getUTCHours(), c.getMinutes(), c.getSeconds()]
+    }).map(pad)
+      .join(':')
 
-var b = new Date()
-b.setHours(arr[1][0])
-b.setMinutes(arr[1][1])
-b.setSeconds(arr[1][2])
-
-var c = new Date(b-a)
-c.getSeconds()
-c.getMinutes()
-var pad = (num) => num < 10 ? `0${num}` : num
-pad(c.getUTCHours())
-
-
-const parseLine = (line) => {
-    let went = false
-    let c = line.indexOf('C'),
-        _ = line.indexOf('_')
-    lastPos = lastPos || _ //T ODO
-    line = line.split('')
-
-    if (-1 !== c) {
-        went = true
-        line.splice(c, 1, go(c))
-    }
-    if (-1 !== _ && !went) {
-        line.splice(_, 1, go(_))
-    }
-    return line.join('')
-}
+const parseLine = (line) =>
+    line.split(' ').sort().map(el => el.split(':'))
 
 const run = (lines) => {
-    return lines.split('\n').map(parseLine).join('\n')
+    return lines.split('\n').map(parseLine).map(timeDiff).join('\n')
 }
 
 module.exports.run = run
