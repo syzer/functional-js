3f6be0576cf392497d52b4f09ea75f627d62a8f0
syzer3@gmail.com <syzer3@gmail.com>
2016-04-09 10:04:02 -0400

    conting sheep implementation

---
+++ code-jam-2016/countingSheep.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const _ = require('lodash')
+let seen
+
+const appendSeen = (num) =>
+    num.toString()
+        .split('')
+        .map(n => parseInt(n, 10))
+        .map(n => {
+            seen[n] = 1
+        })
+
+const isAllSeen = (seen) =>
+    _.reduce(seen, (acc, el, i) => {
+        if (acc && !el) {
+            acc = false
+        }
+        return acc
+    }, true)
+
+const next = (currNum, i) => {
+    appendSeen(currNum)
+    return isAllSeen(seen);
+
+}
+
+const countSheeps = (startNum) => {
+    let i = 0
+    let currNum = i * startNum
+    seen = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+        9: 0
+    }
+
+    // todo: iterator?
+    do {
+        currNum = startNum * (i + 1)
+        i += 1
+    } while ((!next(currNum, i) && i < 1000))
+    console.log(i)
+
+    if (i === 10000) {
+        return 'INSOMNIA'
+    } else {
+        return currNum
+    }
+}
+
+module.exports = countSheeps
