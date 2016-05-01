fad570027ba3b5690cd6f12b35021ef9f2e3d3a5
syzer3@gmail.com <syzer3@gmail.com>
2016-05-01 08:52:49 +0200

    max sum array

--- maxSumArray.py
+++ maxSumArray.py
@@ -16,4 +16,4 @@ def maxSubArraySum(a, size):
 
 
 a = [-2, -3, 4, -1, -2, 1, 5, -3]
-print("Maximum contiguous sum is", maxSubArraySum(a, len(a)))
+print('Maximum contiguous sum is', maxSubArraySum(a, len(a)))
