11dea89114360ba1803749fd45da4e0442f9e22a
syzer3@gmail.com <syzer3@gmail.com>
2016-10-18 09:54:03 +0200

    RX.js multiple observers

---
+++ Rx/1-multiple-observers.js
@@ -0,0 +1,34 @@
+const Rx = require('rx')
+
+const observable = Rx.Observable.interval(500).take(5)
+
+const observerA = {
+    onNext(x) {
+        console.log('A next ' + x)
+        // observerB.next(x)
+    },
+    onError(err) {
+        console.error('A err ' + err)
+    },
+    onCompleted() {
+        console.log('A done ')
+    }
+}
+
+observable.subscribe(observerA)
+
+const observerB = {
+    onNext(x) {
+        console.log('B next ' + x)
+    },
+    onError(err) {
+        console.error('B err ' + err)
+    },
+    onCompleted() {
+        console.log('B done ')
+    }
+}
+
+// setTimeout(() => {
+//     observable.subscribe(observerB)
+// }, 1000)
