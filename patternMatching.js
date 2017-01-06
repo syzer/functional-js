b31aab79616009d2896cfdd67ec5a846b355ee56
syzer3@gmail.com <syzer3@gmail.com>
2017-01-06 18:23:27 +0100

    pattern Maching

--- patternMatching.js
+++ patternMatching.js
@@ -1,17 +1,18 @@
 const _ = require('lodash')
-const func = _.cond([
-    [_.matches({'a': 1}), _.constant('matches A')],
-    [_.conforms({'b': _.isNumber}), _.constant('matches B')],
+const match = _.cond([
+    [_.matches({ 'a': 1 }), _.constant('matches A')],
+    [_.conforms({ 'b': _.isNumber }), _.constant('matches B')],
     [_.constant(true), _.constant('no match')]
 ])
 
-console.log(func({a: 1, b: 2}))
-// → 'matches A'
+match({ a: 1, b: 2 })
+// matches A
 
-console.log(func({a: 0, b: 1}))
-// → 'matches B'
+match({ a: 0, b: 1 })
+// matches B
 
-console.log(func({a: 1, b: '2'}))
-// no matches
+match({ a: 1, b: '2' })
+// matches A
 
-console.log(func({a: 42, b: '2'}))
+match({ a: 42, b: '2' })
+// no match
