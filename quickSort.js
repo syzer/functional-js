4f59484f696cf3c3704a883d26507e34b6ef1234
syzer3@gmail.com <syzer3@gmail.com>
2017-01-12 09:59:30 +0100

    WIP quicksort

---
+++ quickSort.js
@@ -0,0 +1,12 @@
+function quictSort(list) {
+    if (list.isEmpty()) {
+        return list
+    }
+    let pivot = list.first()
+    let remaining = list.rest()
+
+    let lessThanPivot
+}
+
+
+///TODO resonable js 24,in into
