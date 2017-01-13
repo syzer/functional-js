e379885dc276c4117d6366bb7c3886283c231070
syzer3@gmail.com <syzer3@gmail.com>
2017-01-13 09:54:05 +0100

    quicksort

--- quickSort.js
+++ quickSort.js
@@ -1,12 +1,20 @@
-function quictSort(list) {
+const { List } = require('immutable')
+
+const quickSort = (list) => {
     if (list.isEmpty()) {
         return list
     }
+
     let pivot = list.first()
     let remaining = list.rest()
 
-    let lessThanPivot
+    let lessThanPivot = remaining.filter(v => v < pivot)
+    let biggerThanPivot = remaining.filter(v => v >= pivot)
+
+    return quickSort(lessThanPivot)
+        .concat(pivot)
+        .concat(quickSort(biggerThanPivot))
 }
 
-
-///TODO resonable js 24,in into
+quickSort(List([1, 23, 42, 9, 5, 2, 8]))
+// List [ 1, 2, 5, 8, 9, 23, 42 ]
