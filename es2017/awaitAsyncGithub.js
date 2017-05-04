d9199d9d80e933bb4b18058aebe67b2d64e4c5f0
syzer3@gmail.com <syzer3@gmail.com>
2017-05-04 16:04:39 +0200

    github repos

---
+++ es2017/awaitAsyncGithub.js
@@ -0,0 +1,28 @@
+const fetch = require('node-fetch')
+
+async function fetchGithub(endpoint) {
+  const url = `https://syzer:48388bc7012eab4ac481ed3f3b301a11957e2b0f@api.github.com${endpoint}`
+  const response = await fetch(url)
+  return await response.json()
+}
+
+async function showUserAndRepos(handle) {
+  // await promise
+  const user = await fetchGithub(`/users/${handle}`)
+  const repos = await fetchGithub(`/users/${handle}/repos`)
+
+  return { name: user.name, repos }
+}
+
+
+async function showUserAndRepos2(handle) {
+  const [user, repos] = await Promise.all([
+    fetchGithub(`/users/${handle}`),
+    fetchGithub(`/users/${handle}/repos`)
+  ])
+
+  return { name: user.name, repos }
+}
+showUserAndRepos2('syzer')
+  .then(console.log)
+  .catch(console.error)
