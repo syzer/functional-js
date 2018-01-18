2a96488780ae96803a79190250c45ceb7f81a291
syzer3@gmail.com <syzer3@gmail.com>
2018-01-18 22:28:50 +0100

    lensx2

--- lences-ramda.js
+++ lences-ramda.js
@@ -25,3 +25,41 @@ R.pipe(
   R.map(R.view(xLens)),
   R.reject(isFalsy),
 )(objs)
+
+
+// lences composition
+const o = { x: { y: 1 }, y: 2 }
+
+const x = R.lensProp('x')
+const y = R.lensProp('y')
+
+R.view(x, o)  // => { y: 1 }
+R.view(y, o)  // => 2
+
+R.view(R.compose(x, y), o)  // => 1
+
+// lences composition
+const o2 = { x: { }, y: 2 }
+
+const x2 = R.lensPath('x')
+const y2 = R.lensPath('y')
+
+R.view(x2, o2)  // => undefined
+R.view(y2, o2)  // => 2
+
+// YOU can compose lens
+R.view(R.compose(y2, x2), o2)  // => undefined
+
+var dotPath = R.useWith(R.path, [R.split('.')]);
+var propsDotPath = R.useWith(R.ap, [R.map(dotPath), R.of])
+var obj2 = {
+  x: 2,
+  a: { x: 1 }
+}
+
+propsDotPath(['a.x', 'x', 'z'], obj2) // [1, 2]
+
+const firstPath = R.pipe(propsDotPath, R.reject(R.isNil), R.head)
+// (['x', 'a.x'])
+
+// firstPath(['x', 'a.x'], obj2)
