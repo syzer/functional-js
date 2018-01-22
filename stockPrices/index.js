385a62bf22cf88dd244fdb784e8611c45eb07f05
syzer3@gmail.com <syzer3@gmail.com>
2018-01-22 23:11:30 +0100

    WIP: max daily Gain

--- stockPrices/index.js
+++ stockPrices/index.js
@@ -5,7 +5,9 @@
 const assert = require('assert')
 
 const prices = [1, 2, 3, 4, 5]
-const prices2 = [3, 7, 0, 6, 4]
+const prices2 = [4, 8, 1, 2, 7, 5]
+const prices3 = [4, 8, 1, 2, 7, 5, 9]
+// 1, 9
 
 const maxGain = arr => arr.reduce((acc, curr, i, arr) => {
   if (acc.max < curr) {
@@ -28,6 +30,69 @@ const maxGain = arr => arr.reduce((acc, curr, i, arr) => {
   curr: 0,
 })
 
+const raising = []
+let currMax = 0
+let prevMax = 0
+let currMin = 1000000
+let prevMin = 1000000
+
+const maxGain2 = arr => arr.reduce((acc, curr, i, arr) => {
+  if (curr > acc) {
+    raising.push(curr - acc)
+    currMax = curr
+    // return curr - acc
+  }
+
+  if (curr < acc) {
+    if (prevMin < currMin) {
+      prevMin = currMin
+    }
+    currMin = curr
+    return 0
+  }
+
+  console.log(acc, curr, raising, currMax, currMin, prevMin, prevMax)
+  return curr
+}, 0)
+
+const add = (a,b) => a + b
+
+const maxGain3 = acc => acc
+  .map((e, i, arr) =>
+    arr[i - 1]
+      ? e - arr[i - 1]
+      : 0)
+  .map(console.log)
+  .reduce((acc, curr) => {
+    if (curr > 0) {
+      acc.seq.push(curr)
+      if (acc.max < curr) {
+        acc.max = curr
+      }
+      let currMax = acc.seq.reduce(add, 0)
+      if (currMax > acc.max) {
+        acc.max = currMax
+      }
+    } else {
+      // reset while dropping
+      let currMax = acc.seq.reduce(add, 0)
+      if (currMax > acc.max) {
+        acc.max = currMax
+      }
+      acc.seq = []
+    }
+    acc.curr = curr
+    // console.log(curr, acc)
+    return acc
+  }, {
+    seq: [],
+    max: 0,
+    curr: 0
+  })
+
+// console.log(maxGain3(prices2))
+// console.log(maxGain3(prices))
+console.log(maxGain3(prices3))
 
 // assert(maxGain(prices) === 5 - 1)
-assert(maxGain(prices2) === 5 - (-2))
+// assert(maxGain2(prices2) === 5 - (-2))
